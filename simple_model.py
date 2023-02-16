20928435d1d0c3edd361d56b93980cc51535cc63
avizyt <avijit.biswas27@outlook.com>
2023-02-16 20:39:57 +0530

    simple model

---
+++ simple_model.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Define the neural network architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Define the loss function and optimizer
+net = Net().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+epochs = 10
+
+# Load and preprocess the FashionMNIST dataset
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5,), (0.5,))])
+trainset = torchvision.datasets.FashionMNIST(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                          shuffle=True, num_workers=2)
+
+# Train the neural network
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 200 == 199:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200))
+            running_loss = 0.0
+
+print('Finished Training')
+print("Saving Model...")
+torch.save(net, 'net.pt')
+
+print("Starting Evaluation...")
+print("Loading Model ...")
+print("Starting Evaluation Engine...")
+
+model = torch.load("net.pt")
+model.eval()
+
+# Evaluate the performance of the neural network
+transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))]
+        )
+
+testset = torchvision.datasets.FashionMNIST(
+                            root='./data', 
+                            train=False,
+                            download=True, 
+                            transform=transform)
+
+testloader = torch.utils.data.DataLoader(
+                            testset, 
+                            batch_size=64,
+                            shuffle=False, 
+                            num_workers=2)
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
